be5c4927392b4b4a85936d9aaa77c66c0ea2f5d5
Carter Fluckiger <fluckiger2@tritions.iowacentral.edu>
2020-01-30 18:28:23 -0600

    Initial Commit

---
+++ Semester Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Semester Project
+//
+//  Created by Tag1 on 1/30/20.
+//  Copyright © 2020 Lab-Final. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
